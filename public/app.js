fc20560ccd5e02b1eb73d3839fb49a987a16b5f2
unknown <pavelko1608@gmail.com>
2018-01-12 18:29:30 +0200

    enhanced the component's structure

--- public/app.js
+++ public/app.js
@@ -1,52 +1,94 @@
-function Application() {
+const PLAYERS = [{
+  name: "Jim Blunt",
+  score: 34,
+  id: 1
+}, {
+  name: "Sarah Blunt",
+  score: 54,
+  id: 2
+}];
+
+function Header(props) {
   return React.createElement(
     "div",
-    { className: "scoreboard" },
+    { className: "header" },
     React.createElement(
-      "div",
-      { className: "header" },
-      React.createElement(
-        "h1",
-        null,
-        "Scoreboard"
-      )
+      "h1",
+      null,
+      props.title
+    )
+  );
+  Header.propTypes = {
+    title: React.PropTypes.string.isRequired
+  };
+}
+function Counter(props) {
+  return React.createElement(
+    "div",
+    { className: "counter" },
+    React.createElement(
+      "button",
+      { className: "counter-action decrement" },
+      " - "
     ),
     React.createElement(
       "div",
-      { className: "players" },
-      React.createElement(
-        "div",
-        { className: "player" },
-        React.createElement(
-          "div",
-          { className: "player-name" },
-          "Paul Paliychuk"
-        ),
-        React.createElement(
-          "div",
-          { className: "player-scrore" },
-          React.createElement(
-            "div",
-            { className: "counter" },
-            React.createElement(
-              "button",
-              { className: "counter-action decrement" },
-              " - "
-            ),
-            React.createElement(
-              "div",
-              { className: "counter-score" },
-              " 31 "
-            ),
-            React.createElement(
-              "button",
-              { className: "counter-action increment" },
-              " + "
-            )
-          )
-        )
-      )
+      { className: "counter-score" },
+      " ",
+      props.score,
+      " "
+    ),
+    React.createElement(
+      "button",
+      { className: "counter-action increment" },
+      " + "
     )
   );
+  Counter.propTypes = {
+    score: React.PropTypes.number.isRequired
+  };
 }
-ReactDOM.render(React.createElement(Application, null), document.getElementById("container"));
+function Player(props) {
+  return React.createElement(
+    "div",
+    { className: "player" },
+    React.createElement(
+      "div",
+      { className: "player-name" },
+      props.name
+    ),
+    React.createElement(
+      "div",
+      { className: "player-scrore" },
+      React.createElement(Counter, { score: props.score })
+    )
+  );
+  Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired
+  };
+}
+function Application(props) {
+  return React.createElement(
+    "div",
+    { className: "scoreboard" },
+    React.createElement(Header, { title: props.title }),
+    React.createElement(
+      "div",
+      { className: "players" },
+      props.players.map(player => {
+        return React.createElement(Player, { name: player.name, score: player.score, key: player.id });
+      })
+    )
+  );
+  Application.propTypes = {
+    players: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired
+    })).isRequired
+  };
+}
+
+ReactDOM.render(React.createElement(Application, { title: "My scoreboard", players: PLAYERS }), document.getElementById("container"));
--- public/app.jsx
+++ public/app.jsx
@@ -1,25 +1,74 @@
-function Application() {
-  return (
-    <div className="scoreboard">
-      <div className="header">
-        <h1>Scoreboard</h1>
-      </div>
+const PLAYERS = [
+  {
+    name: "Jim Blunt",
+    score: 34,
+    id: 1
+  },
+  {
+    name: "Sarah Blunt",
+    score: 54,
+    id: 2
+  }
+]
 
-      <div className="players">
-        <div className="player">
-          <div className="player-name">
-            Paul Paliychuk
-          </div>
-          <div className="player-scrore">
-            <div className="counter">
-              <button className="counter-action decrement"> - </button>
-              <div className="counter-score"> 31 </div>
-              <button className="counter-action increment"> + </button>
-            </div>
-          </div>
-        </div>
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
+  )
+  Header.propTypes = {
+    title: React.PropTypes.string.isRequired
+  }
+}
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score"> {props.score} </div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
+  Counter.propTypes = {
+    score: React.PropTypes.number.isRequired
+  }
+}
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-scrore">
+        <Counter score={props.score} />
       </div>
     </div>
   )
+  Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired
+  }
 }
-ReactDOM.render(<Application />, document.getElementById("container"))
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <Header title={props.title} />
+      <div className="players">
+        {props.players.map((player) => {
+            return <Player name={player.name} score={player.score} key={player.id} />
+          })
+        }
+      </div>
+    </div>
+  )
+  Application.propTypes = {
+    players: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired
+    })).isRequired
+  }
+}
+
+ReactDOM.render(<Application title="My scoreboard" players={PLAYERS}/>, document.getElementById("container"))
